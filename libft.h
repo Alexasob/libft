911e7c47e2ddf40b9823f62ce2d4829e9a3cef30
Alexasob <96636096+Alexasob@users.noreply.github.com>
2026-01-09 22:58:00 +0300

    ilk guncelleme

---
+++ libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asobolev <asobolev@student.42istanbul.com.tr +#+  +:+       +#+      */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 21:55:22 by asobolev          #+#    #+#             */
+/*   Updated: 2026/01/09 21:59:02 by asobolev         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalnum(int a);
+int	ft_isalpha(int a);
+int	ft_isdigit(int a);
+
+#endif
