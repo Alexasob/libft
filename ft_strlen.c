187ca2ffad918396611e69d0e7d20b5abc861faa
Alexasob <96636096+Alexasob@users.noreply.github.com>
2026-01-10 04:00:43 +0300

    ft-strlen eklendi

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asobolev <asobolev@student.42istanbul.com.tr +#+  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/09 23:15:28 by asobolev          #+#    #+#             */
-/*   Updated: 2026/01/09 23:44:50 by asobolev         ###   ########.tr       */
+/*   Created: 2026/01/09 23:45:52 by asobolev          #+#    #+#             */
+/*   Updated: 2026/01/10 03:50:19 by asobolev         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-int	main(void)
+size_t	ft_strlen(const char *s)
 {
-	printf("%d", ft_isprint(128));
-	return (0);
+	const char	*p = s;
+
+	while (*p++)
+		;
+	return (p - s - 1);
+}
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	char dizii[]="keremnaber la";
+	printf("%zu",ft_strlen(dizii));
 }
--- libft.h
+++ libft.h
@@ -6,17 +6,19 @@
 /*   By: asobolev <asobolev@student.42istanbul.com.tr +#+  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 21:55:22 by asobolev          #+#    #+#             */
-/*   Updated: 2026/01/09 23:35:57 by asobolev         ###   ########.tr       */
+/*   Updated: 2026/01/10 03:52:40 by asobolev         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stdlib.h>
 
 int	ft_isalnum(int a);
 int	ft_isalpha(int a);
 int	ft_isdigit(int a);
 int	ft_isascii(int a);
 int	ft_isprint(int a);
+size_t	ft_strlen(const char *s);
 
 #endif
