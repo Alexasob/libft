77c6b5ac08dfd4cf9382fa4e2273264859d44500
Alexasob <96636096+Alexasob@users.noreply.github.com>
2026-01-14 05:07:13 +0300

    verimsiz sabahlama sonucum

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asobolev <asobolev@student.42istanbul.com.tr +#+  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/09 21:55:22 by asobolev          #+#    #+#             */
-/*   Updated: 2026/01/14 05:01:15 by asobolev         ###   ########.tr       */
+/*   Created: 2026/01/14 03:05:39 by asobolev          #+#    #+#             */
+/*   Updated: 2026/01/14 04:52:24 by asobolev         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+#include "libft.h"
 
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
 
-int		ft_isalnum(int a);
-int		ft_isalpha(int a);
-int		ft_isdigit(int a);
-int		ft_isascii(int a);
-int		ft_isprint(int a);
-size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-
-#endif
+/*
+#include <stdio.h>
+int main ()
+{
+	int dizi[]={1,2,3,4,5,6,};
+	ft_bzero(dizi,2);
+	printf("%s",dizi);
+}*/
---
+++ ft_memset.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asobolev <asobolev@student.42istanbul.com.tr +#+  +:+       +#+      */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 04:47:52 by asobolev          #+#    #+#             */
+/*   Updated: 2026/01/14 03:25:31 by asobolev         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*a;
+	size_t			i;
+
+	i = 0;
+	a = (unsigned char *)s;
+	while (n != i)
+	{
+		a[i++] = (unsigned char)c;
+	}
+	return (s);
+}
+
+/* #include <stdio.h>
+int main()
+{
+	char dizi[]="1,2,3,4,5,6,7,8,9,";
+	
+	ft_memset(dizi,'1',5);
+	printf("%s",dizi);
+}
+// memset byte byte yazmalı o yuzden char kullanıyoruz  int(4 byt) char (1 byt )
+cast  geçiçi bir işlem belleğe atamzsan kayboluyr */ 
