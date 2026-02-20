e9a331f4943a47f4c85adb2c8c41c9a173f04402
Alexasob <96636096+Alexasob@users.noreply.github.com>
2026-02-20 15:12:46 +0300

    rename wrong variable name

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: asobolev <asobolev@student.42istanbul.com.t+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 16:10:14 by asobolev          #+#    #+#             */
-/*   Updated: 2026/02/19 05:49:18 by asobolev         ###   ########.fr       */
+/*   Updated: 2026/02/20 15:12:27 by asobolev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	lenght;
+	size_t	length;
 	size_t	i;
 
 	i = 0;
-	lenght = ft_strlen(src);
+	length = ft_strlen(src);
 	if (size == 0)
 	{
-		return (lenght);
+		return (length);
 	}
 	while (i < size - 1 && src[i] != '\0')
 	{
@@ -29,5 +29,5 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 		i++;
 	}
 	dst[i] = '\0';
-	return (lenght);
+	return (length);
 }
